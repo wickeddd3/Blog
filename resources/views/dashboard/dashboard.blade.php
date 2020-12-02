dace1091fd38afcebb05da3feafde4bd5389f328
wickedwicked <47551506+wickeddd3@users.noreply.github.com>
2020-12-02 16:03:05 +0800

    code refactor and UI update using custom SASS phase 2

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,79 +1,87 @@
 @extends('dashboard.layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="dashboard">
     <h5 class="heading-primary">Dashboard</h5>
-    {{-- <div class="pt-2">
 
-        <div class="row">
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box">
-                <span class="info-box-icon bg-info elevation-1"><i class="fa fa-book"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Posts</span>
-                    <span class="info-box-number">{{ $postsCount }}</span>
+    <div class="container__row">
+        <div class="container__col-xl-3 container__col-12">
+            <div class="widget">
+                <div class="widget__content">
+                    <div class="widget__body">
+                        <div class="widget__total">
+                            <div class="widget__title">Posts</div>
+                            <div class="widget__number">{{ $postsCount }}</div>
+                        </div>
+                        <i class="fa fa-book widget__icon"></i>
+                    </div>
+                    <div class="widget__footer">
+                        Since last month
+                    </div>
                 </div>
-
-                </div>
-
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                <span class="info-box-icon bg-primary elevation-1"><i class="fa fa-users"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Users</span>
-                    <span class="info-box-number">{{ $usersCount }}</span>
-                </div>
-
-                </div>
-
-            </div>
-
-
-            <div class="clearfix hidden-md-up"></div>
-
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                <span class="info-box-icon bg-success elevation-1"><i class="fa fa-archive"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Categories</span>
-                    <span class="info-box-number">{{ $categoriesCount }}</span>
-                </div>
-
-                </div>
-
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                <span class="info-box-icon bg-secondary elevation-1"><i class="fa fa-tags"></i></span>
-
-                <div class="info-box-content">
-                    <span class="info-box-text">Tags</span>
-                    <span class="info-box-number">{{ $tagsCount }}</span>
-                </div>
-
-                </div>
-
-            </div>
-
-        </div>
-
-
-
-        <div class="pt-2">
-            <div class="jumbotron">
-                <h1 class="display-4">Hello, {{ Auth::user()->full_name }}!</h1>
-                <p class="lead">Share your insights and ideas to everyone around the world.</p>
-                <hr class="my-4">
-                <p>Create your new post today, click create new post.</p>
-                <a class="btn btn-primary btn-lg" href="{{ route('post.add') }}" role="button">Create new post</a>
             </div>
         </div>
-    </div> --}}
+        <div class="container__col-xl-3 container__col-12">
+            <div class="widget">
+                <div class="widget__content">
+                    <div class="widget__body">
+                        <div class="widget__total">
+                            <div class="widget__title">Users</div>
+                            <div class="widget__number">{{ $usersCount }}</div>
+                        </div>
+                        <i class="fa fa-users widget__icon"></i>
+                    </div>
+                    <div class="widget__footer">
+                        Since last month
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container__col-xl-3 container__col-12">
+            <div class="widget">
+                <div class="widget__content">
+                    <div class="widget__body">
+                        <div class="widget__total">
+                            <div class="widget__title">Categories</div>
+                            <div class="widget__number">{{ $categoriesCount }}</div>
+                        </div>
+                        <i class="fa fa-archive widget__icon"></i>
+                    </div>
+                    <div class="widget__footer">
+                        Since last month
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container__col-xl-3 container__col-12">
+            <div class="widget">
+                <div class="widget__content">
+                    <div class="widget__body">
+                        <div class="widget__total">
+                            <div class="widget__title">Tags</div>
+                            <div class="widget__number">{{ $tagsCount }}</div>
+                        </div>
+                        <i class="fa fa-tags widget__icon"></i>
+                    </div>
+                    <div class="widget__footer">
+                        Since last month
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="m-t-2">
+        <div class="container--fluid">
+            <h1 class="heading-title">Hello, {{ Auth::user()->full_name }}!</h1>
+            <p class="paragraph m-t-1">Share your insights and ideas to everyone around the world.</p>
+            <p class="paragraph m-t-1">Create your new post today, click create new post.</p>
+            <p class="m-t-2">
+                <a class="btn btn--primary heading-secondary" href="{{ route('post.add') }}" role="button">Create new post</a>
+            </p>
+        </div>
+    </div>
+
+
 </div>
 @endsection
