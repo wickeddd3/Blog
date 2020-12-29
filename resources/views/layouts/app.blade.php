54b35d33f8ad83be8c99aaeb1f0c4d8e4616dbed
wickedwicked <47551506+wickeddd3@users.noreply.github.com>
2020-12-29 14:55:19 +0800

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Blog</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script>
             window.App = {!! json_encode([
-                //    'csrfToken' => csrf_token(),
                 'user' => Auth::id(),
                 'verified' => Auth::check() ? Auth::user()->email_verified_at : false,
                 'signedIn' => Auth::check()
